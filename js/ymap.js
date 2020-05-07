f3c9685820aded7595afb805dd9691ca7b4c7aa5
Artyom <Psixart1@gmail.com>
2020-05-07 23:18:38 +0300

    foter&map add

---
+++ js/ymap.js
@@ -0,0 +1,10 @@
+YMaps.jQuery(function () {
+  var map = new YMaps.Map(YMaps.jQuery("#YMapsID")[0]);
+  map.setCenter(new YMaps.GeoPoint(37.610877, 55.729556), 14);
+  var s = new YMaps.Style();
+  s.iconStyle = new YMaps.IconStyle();
+  s.iconStyle.href = "../img/svg/pin-yandex.svg";
+  s.iconStyle.offset = new YMaps.Point(-9, -29);
+  var placemark = new YMaps.Placemark(new YMaps.GeoPoint(37.631624, 55.730666), {style: s});
+  map.addOverlay(placemark); 
+})
