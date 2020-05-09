9724f7bde3361673fa87f663bf9865c072db0c70
Artyom <Psixart1@gmail.com>
2020-05-10 01:33:23 +0300

    complete: descktop version, responsive, validation

--- js/ymap.js
+++ js/ymap.js
@@ -3,7 +3,7 @@ YMaps.jQuery(function () {
   map.setCenter(new YMaps.GeoPoint(37.610877, 55.729556), 14);
   var s = new YMaps.Style();
   s.iconStyle = new YMaps.IconStyle();
-  s.iconStyle.href = "../img/svg/pin-yandex.svg";
+  s.iconStyle.href = "./img/svg/pin-yandex.svg";
   s.iconStyle.offset = new YMaps.Point(-9, -29);
   var placemark = new YMaps.Placemark(new YMaps.GeoPoint(37.631624, 55.730666), {style: s});
   map.addOverlay(placemark); 
